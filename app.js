e692637034eac1cc44d94310d14aa89a72ded8dd
torreseam <615694479+torreseam@users_noreply.github.com>
2020-05-10 22:51:01 -0500

    update app js

---
+++ app.js
@@ -0,0 +1,24 @@
+// var message = 'Hello Node!';
+
+// var sum = 5 + 3;
+
+// console.log(message);
+// console.log(sum);
+
+var commandLineArgs = process.argv;
+console.log(commandLineArgs);
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+
+        console.log('================');
+
+        profileDataArr.forEach(profileItem => console.log(profileItem));
+        });
+    }
+};
