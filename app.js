5d26f8df7513d9fedb7531e4031fc52394cfa052
torreseam <615694479+torreseam@users_noreply.github.com>
2020-05-12 23:40:54 -0500

    update inquirer and moment nmp

--- app.js
+++ app.js
@@ -1,12 +1,59 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// console.log(inquirer)
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2);
 
-const [name, github] = profileDataArgs;
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const profileDataArgs = process.argv.slice(2);
+
+// const [name, github] = profileDataArgs;
+
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
